5383b75a8ff2e3efc5ea8fb088183cbc243f3a69
Ratan Masanta <ratanmasanta@gmail.com>
2022-03-05 19:12:53 +0530

    Binary search code

---
+++ src/com/masanta/ratan/leetcode/study/plan/algorithms/one/BinarySearch.java
@@ -0,0 +1,10 @@
+package com.masanta.ratan.leetcode.study.plan.algorithms.one;
+
+public class BinarySearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
