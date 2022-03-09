aecf9ae8f1ad22c4e48bc0728931dd3d0cb0aaea
Ratan Masanta <ratanmasanta@gmail.com>
2022-03-09 07:14:37 +0530

    Added ListNode class for LinkedLists

---
+++ src/com/masanta/ratan/leetcode/study/plan/algorithms/one/ListNode.java
@@ -0,0 +1,12 @@
+package com.masanta.ratan.leetcode.study.plan.algorithms.one;
+
+public class ListNode {
+
+	int val;
+	ListNode next;
+	ListNode() {}
+	ListNode(int val) { this.val = val; }
+	ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+
+}
