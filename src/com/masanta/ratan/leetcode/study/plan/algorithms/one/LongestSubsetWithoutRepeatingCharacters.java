5d75749f83a0d60dfc2e3d5557d2e8c98eaf5cc7
Ratan Masanta <ratanmasanta@gmail.com>
2022-03-11 09:46:17 +0530

    Longest subset without repeating characters using Sliding Window technique.

---
+++ src/com/masanta/ratan/leetcode/study/plan/algorithms/one/LongestSubsetWithoutRepeatingCharacters.java
@@ -0,0 +1,42 @@
+package com.masanta.ratan.leetcode.study.plan.algorithms.one;
+
+public class LongestSubsetWithoutRepeatingCharacters {
+
+	public static void main(String[] args) {
+		System.out.println(lengthOfLongestSubstring("asacajsbayhvwelancacndfgnfkgr"));
+
+	}
+	
+	public static int lengthOfLongestSubstring(String s) {
+		// we begin by breaking the string into char array to process it faster
+        char[] charSet = s.toCharArray();
+        int n = charSet.length;
+        if (n == 0) return 0; // exception case for faster performance
+        int length = 1; // current length
+        int longest = 1; // longest length so far
+        int left = 0; // left index of our sliding window
+        int tmp = 0; // index of the duplicate
+        for (int right = 1; right < n; right++) {  // we start by moving our right side of the window from 1 to the end
+            tmp = charEqualsToCharAtThisIndex(charSet[right], charSet, left, right); 
+			// if the char itself has existed in our sliding window, 
+			// we will slide the left side of the window to the next position of the duplicate 
+            if (tmp == -1) {
+                length++;
+                longest = longest > length ? longest : length;
+            } else {
+                length = right - tmp;
+                left = tmp + 1;
+            }
+        }
+        return longest;
+    }
+    
+    private static int charEqualsToCharAtThisIndex(char c, char[] seq, int firstIndex, int lastIndex) {
+        for (int i = firstIndex; i < lastIndex; i++) {
+            if (c == seq[i]) 
+                return i;
+        }
+        return -1;
+    }
+
+}
