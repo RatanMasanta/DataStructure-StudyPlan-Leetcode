274df10cc857e8db57342f030cced685c77ad140
Ratan Masanta <ratanmasanta@gmail.com>
2022-03-19 12:01:17 +0530

    Merge intervals problem

---
+++ src/com/masanta/ratan/leetcode/study/plan/data/structure/two/MergeIntervals.java
@@ -0,0 +1,10 @@
+package com.masanta.ratan.leetcode.study.plan.data.structure.two;
+
+public class MergeIntervals {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
