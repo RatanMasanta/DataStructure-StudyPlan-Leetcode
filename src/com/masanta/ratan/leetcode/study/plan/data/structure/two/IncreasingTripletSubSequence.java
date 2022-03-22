42f71e70b969c66d57c5b956ba381aefd9d5fa45
Ratan Masanta <ratanmasanta@gmail.com>
2022-03-22 08:06:20 +0530

    Increasing triplet subsequence

---
+++ src/com/masanta/ratan/leetcode/study/plan/data/structure/two/IncreasingTripletSubSequence.java
@@ -0,0 +1,21 @@
+package com.masanta.ratan.leetcode.study.plan.data.structure.two;
+
+public class IncreasingTripletSubSequence {
+
+	public static void main(String[] args) {
+		System.out.println(increasingTriplet(new int[] {2,1,5,0,4,6}));
+
+	}
+	
+	 public static boolean increasingTriplet(int[] nums) {
+         // start with two largest values, as soon as we find a number bigger than both, while both have been updated, return true.
+        int small = Integer.MAX_VALUE, big = Integer.MAX_VALUE;
+        for (int n : nums) {
+            if (n <= small) { small = n; } // update small if n is smaller than both
+            else if (n <= big) { big = n; } // update big only if greater than small but smaller than big
+            else return true; // return if you find a number bigger than both
+        }
+        return false;
+    }
+
+}
