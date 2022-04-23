8f7b401b69c6cafe9dc87fd5247da7f046762cc0
Ratan Masanta <ratanmasanta@gmail.com>
2022-04-23 18:13:52 +0530

    Given an m x n matrix board containing 'X' and 'O', capture all regions that are 4-directionally surrounded by 'X'

---
+++ src/com/masanta/ratan/leetcode/study/plan/algorithms/two/SurroundedRegions.java
@@ -0,0 +1,66 @@
+package com.masanta.ratan.leetcode.study.plan.algorithms.two;
+
+public class SurroundedRegions {
+
+	/*
+	 * Given an m x n matrix board containing 'X' and 'O', capture all regions that
+	 * are 4-directionally surrounded by 'X'.
+	 * 
+	 * A region is captured by flipping all 'O's into 'X's in that surrounded
+	 * region.
+	 */
+	public static void main(String[] args) {
+		char[][] board= new char [][] {{'X','X','X','X'},{'X','O','O','X'},{'X','X','O','X'},{'X','O','X','X'}};
+		solve(board);
+		System.out.println("Done");
+	}
+
+
+	public static void solve(char[][] board) {
+		if (board.length == 0 || board[0].length == 0)
+			return;
+		if (board.length < 2 || board[0].length < 2)
+			return;
+		int m = board.length, n = board[0].length;
+		//Any 'O' connected to a boundary can't be turned to 'X', so ...
+		//Start from first and last column, turn 'O' to '*'.
+		for (int i = 0; i < m; i++) {
+			if (board[i][0] == 'O')
+				boundaryDFS(board, i, 0);
+			if (board[i][n-1] == 'O')
+				boundaryDFS(board, i, n-1);	
+		}
+		//Start from first and last row, turn '0' to '*'
+		for (int j = 0; j < n; j++) {
+			if (board[0][j] == 'O')
+				boundaryDFS(board, 0, j);
+			if (board[m-1][j] == 'O')
+				boundaryDFS(board, m-1, j);	
+		}
+		//post-prcessing, turn 'O' to 'X', '*' back to 'O', keep 'X' intact.
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (board[i][j] == 'O')
+					board[i][j] = 'X';
+				else if (board[i][j] == '*')
+					board[i][j] = 'O';
+			}
+		}
+	}
+	
+	//Use DFS algo to turn internal however boundary-connected 'O' to '*';
+	private static void boundaryDFS(char[][] board, int i, int j) {
+		if (i < 0 || i > board.length - 1 || j <0 || j > board[0].length - 1)
+			return;
+		if (board[i][j] == 'O')
+			board[i][j] = '*';
+		if (i > 1 && board[i-1][j] == 'O')
+			boundaryDFS(board, i-1, j);
+		if (i < board.length - 2 && board[i+1][j] == 'O')
+			boundaryDFS(board, i+1, j);
+		if (j > 1 && board[i][j-1] == 'O')
+			boundaryDFS(board, i, j-1);
+		if (j < board[i].length - 2 && board[i][j+1] == 'O' )
+			boundaryDFS(board, i, j+1);
+	}
+}
