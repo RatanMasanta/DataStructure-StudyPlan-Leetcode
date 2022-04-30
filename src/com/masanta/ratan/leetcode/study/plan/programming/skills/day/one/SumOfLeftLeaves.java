a2c1f0984a58e9ab5e658fb5215fe912bca256ac
Ratan Masanta <ratanmasanta@gmail.com>
2022-04-30 13:26:45 +0530

    Added TreeNode class

---
+++ src/com/masanta/ratan/leetcode/study/plan/programming/skills/day/one/SumOfLeftLeaves.java
@@ -0,0 +1,10 @@
+package com.masanta.ratan.leetcode.study.plan.programming.skills.day.one;
+
+public class SumOfLeftLeaves {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
