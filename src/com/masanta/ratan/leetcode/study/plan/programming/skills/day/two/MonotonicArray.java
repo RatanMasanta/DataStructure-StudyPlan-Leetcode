7b4341d253992d9dd5ac255c0105ee7f7a8d4135
Ratan Masanta <ratanmasanta@gmail.com>
2022-05-13 22:56:26 +0530

    Check if an array is monotonic or not

--- src/com/masanta/ratan/leetcode/study/plan/programming/skills/day/two/MonotonicArray.java
+++ src/com/masanta/ratan/leetcode/study/plan/programming/skills/day/two/MonotonicArray.java
@@ -4,8 +4,12 @@ public class MonotonicArray {
 
 	public static void main(String[] args) {
 		System.out.println(isMonotonic(new int[] {1,2,3,4,7,6}));
-
+		System.out.println(isMonotonic2(new int[] {1}));
 	}
+	
+	
+	//TC = O(N) + O(2)
+	//SC = O(1)
 	public static boolean isMonotonic(int[] nums) {
         int first = nums[0];
         int last = nums[nums.length - 1];
@@ -23,6 +27,28 @@ public class MonotonicArray {
             }
         }
         return true;
+    }
+	
+	/*
+	 * TC = O(N)
+	 * SC = O(1)
+	 * 
+	 */
+	
+	public static boolean isMonotonic2(int[] A) {
+        boolean increase = false;
+        boolean decrease = false;
+        
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] < A[i - 1]) {
+                decrease = true;
+            }
+            if (A[i] > A[i - 1]) {
+                increase = true;
+            }
+        }
+        
+        return increase && decrease ? false : true;
     }
 
 }
