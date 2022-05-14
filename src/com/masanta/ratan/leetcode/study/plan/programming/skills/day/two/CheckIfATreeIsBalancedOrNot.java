54793b43aabf4b5809cd32c3413fd70797b40119
Ratan Masanta <ratanmasanta@gmail.com>
2022-05-14 11:50:27 +0530

    Added treenode class and Check if a tree is balanced or not

---
+++ src/com/masanta/ratan/leetcode/study/plan/programming/skills/day/two/CheckIfATreeIsBalancedOrNot.java
@@ -0,0 +1,10 @@
+package com.masanta.ratan.leetcode.study.plan.programming.skills.day.two;
+
+public class CheckIfATreeIsBalancedOrNot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
