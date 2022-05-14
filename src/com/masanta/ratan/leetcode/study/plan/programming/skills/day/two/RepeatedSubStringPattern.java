6a58ccc7c41f6634b1e9e3166bf2f42d3b5f56b1
Ratan Masanta <ratanmasanta@gmail.com>
2022-05-14 18:57:29 +0530

    Check if a string is used repeatedly to create the said string

---
+++ src/com/masanta/ratan/leetcode/study/plan/programming/skills/day/two/RepeatedSubStringPattern.java
@@ -0,0 +1,34 @@
+package com.masanta.ratan.leetcode.study.plan.programming.skills.day.two;
+
+public class RepeatedSubStringPattern {
+
+	public static void main(String[] args) {
+		System.out.println(repeatedSubstringPattern("abcabcabcabc"));
+
+	}
+	
+    public static boolean repeatedSubstringPattern(String s) {
+        // corner case
+        if(s == null || s.length() == 0) return true;
+        
+        int n = s.length();
+        for(int len = 1; len <= n/2; len++){ // pattern must repeat at least twice, i.e. pattern length is at most n/2
+            if(n % len != 0) continue; // s length must can be divided by the pattern length
+            
+            String pattern = s.substring(0, len); // pattern string
+            int i = len; // start index of 2nd pattern
+            int j = i + len - 1; // end index of 2nd pattern
+            while(j < n){
+                String substr = s.substring(i, j + 1);
+                if(!pattern.equals(substr)) break; // failed for this pattern, try next pattern		
+                i += len;
+                j += len;
+            }
+			
+			// if it past the last substring check, i will be n
+            if(i == n) return true;
+        }
+        return false;
+    }
+
+}
