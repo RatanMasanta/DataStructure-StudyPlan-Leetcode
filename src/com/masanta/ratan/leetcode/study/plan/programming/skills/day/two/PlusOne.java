317dd545270f453f0c955c7b030bcf712155210f
Ratan Masanta <ratanmasanta@gmail.com>
2022-05-15 12:08:56 +0530

    Added second method

--- src/com/masanta/ratan/leetcode/study/plan/programming/skills/day/two/PlusOne.java
+++ src/com/masanta/ratan/leetcode/study/plan/programming/skills/day/two/PlusOne.java
@@ -6,6 +6,7 @@ public class PlusOne {
 
 	public static void main(String[] args) {
 		System.out.println(Arrays.toString(plusOne(new int[] {4,3,2,1,9,9,8})));
+		System.out.println(Arrays.toString(plusOne2(new int[] {4,3,2,1,9,9,8})));
 
 	}
 
@@ -25,6 +26,20 @@ public class PlusOne {
 
 		return newNumber;
 
+	}
+	
+	public static int[] plusOne2(int[] digits) {
+	    int index;
+	    for (index = digits.length - 1; index >= 0 && digits[index] == 9; index--) {    // cases with 9
+	        digits[index] = 0;
+	    }
+	    if (index == -1) {  // edge case for all digits are 9, so becomes 1 with n zeroes
+	        int[] newDigits = new int[digits.length + 1];
+	        newDigits[0] = 1;   // new int[] initialise with zeroes, so set first digit as 1
+	        return newDigits;
+	    }
+	    digits[index]++;    // found the non 9 digit, just add 1
+	    return digits;
 	}
 
 }
